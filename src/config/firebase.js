48c022adf893e77bdb0203f19badb2df5ee8e0b7
ArZuStr <gulijevaa@gmail.com>
2023-04-04 14:37:27 +0300

    installed Firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+
+
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+    import {getAuth} from 'firebase/auth';
+
+
+
+    const firebaseConfig = {
+    apiKey: "AIzaSyAOhBJU3TOaW67Pt5O0hdqmWvQSWAUkdWo",
+    authDomain: "sparts-app-website.firebaseapp.com",
+    projectId: "sparts-app-website",
+    storageBucket: "sparts-app-website.appspot.com",
+    messagingSenderId: "232067063522",
+    appId: "1:232067063522:web:5987ed8977ebe75e2cc8bb"
+};
+
+    const app = initializeApp(firebaseConfig);
+</script>
