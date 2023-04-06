842b187da3dfc5a7b12aea13c0e1419850288d10
Kaisa <kaisakalmet@gmail.com>
2023-04-06 14:18:42 +0300

    added useEffect to fetch users and events from Firebase. Added map.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
+import 'leaflet/dist/leaflet.css'
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
+import L from 'leaflet'
 
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -10,12 +13,10 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Box from '@mui/material/Box';
 
-
 import {db} from './config/firebase';
 import {collection, getDocs, addDoc} from "firebase/firestore";
 
 // import NonUserEventComponent from "./Components/NonUserEventComponent.js"
-
 
 // export default function FormDialog() {
 //     const [open, setOpen] = React.useState(false);
@@ -23,12 +24,17 @@ import {collection, getDocs, addDoc} from "firebase/firestore";
 //     const [users, setUsers] = useState([])
 //     const handleClickOpen = () => {
 //         fetchUsers();
-//
 //         setOpen(true);
 //     };
 // }
 
+delete L.Icon.Default.prototype._getIconUrl;
 
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 //kaisa comment
 function App() {
@@ -37,14 +43,13 @@ function App() {
     const [open1, setOpen1] = useState(false);
     const [users, setUsers] = useState(false);
     const [events, setEvents] = useState(false);
+    const [location, setLocation] = useState([]);
 
     const handleClose = () => {
         setOpen(false);
     };
 
     const handleClickOpen = () => {
-
-
         setOpen(true);
     };
 
@@ -76,27 +81,68 @@ function App() {
             })
     }
 
+    async function fetchLocation() {
+        await getDocs(collection(db, 'Events.location'))
+            .then((querySnapshot) => {
+                const newData = querySnapshot.docs
+                    .map((doc) => ({...doc.data(), id: doc.id}));
+                setLocation(newData);
+            })
+    }
+
     useEffect(() => {
         // Update the data (users. events) from Firestore
         fetchUsers();
         fetchEvents();
-    }, []); //empty deps make it run only once
+        fetchLocation();
+    }, []); //empty deps makes it run only once
 
     console.log(users)
     console.log(events)
-
+    console.log(location)
 
     return (
 
-        <Box sx={{width:"100%"}}>
+        <div style={{ position: "relative", height: "100vh" }}>
+        <MapContainer
+            className="full-screen-map"
+            center={[59.436962, 24.753574]}
+            zoom={12}
+            minZoom={3}
+            maxZoom={19}
+            maxBounds={[[-85.06, -180], [85.06, 180]]}
+            scrollWheelZoom={true}>
+            <TileLayer
+                attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+                url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png" />
+
+            {location.map((location, index) => (
+                <Marker
+                    key={index}
+                    position={[location.lngLat[1], location.lngLat[0]]}>
+                    <Popup>
+                        {location.name}<br />
+                    </Popup>
+                </Marker>
+            ))}
+        </MapContainer>
+
+        <Box sx={{position: "absolute",
+            top: "10px",
+            right: "10px",
+            zIndex: "1000",
+            display: "flex",
+            flexDirection: "row",}}>
             <Box display="flex" flexDirection="row" justifyContent="flex-end" sx={{width:"100%"}}>
-                <Button variant="outlined" onClick={handleClickOpen}>
+                <Button variant="contained" onClick={handleClickOpen}>
                    LOG IN
                 </Button>
-                <Button variant="outlined" onClick={handleClickOpen1}>
+                <Button variant="contained" onClick={handleClickOpen1}>
                     SIGN UP
                 </Button>
             </Box>
+        </Box>
+
             <Dialog open={open} onClose={handleClose}>
                 <DialogTitle>Log in</DialogTitle>
                 <DialogContent>
@@ -183,7 +229,7 @@ function App() {
                     <Button onClick={handleClose1}>Create</Button>
                 </DialogActions>
             </Dialog>
-        </Box>
+        </div>
     );
 }
 
